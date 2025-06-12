8d1927570138e5fbb0b3c60d365026fb1da3c6b5
shivamThakurXcode <shivam.thakur.official40@gmail.com>
2025-06-12 15:47:20 +0530

    footer style addded

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,7 +15,12 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-cover bg-[#0e1d06] bg-center ('./public/footer-bg.svg')] w-full mx-auto   pt-16 pb-8 border-t  border-gray-800">
+    <footer
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url('./footer-bg.svg')`,
+      }}
+      className="bg-cover bg-[#0e1d06] bg-center ('./public/footer-bg.svg')] w-full mx-auto   pt-16 pb-8 border-t  border-gray-800"
+    >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           {/* Brand and Description */}
