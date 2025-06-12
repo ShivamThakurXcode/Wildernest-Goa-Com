333e64ffadeed178ddda546fe9bf4dc42836faa3
shivamThakurXcode <shivam.thakur.official40@gmail.com>
2025-06-12 15:58:09 +0530

    cta svg problem still occured

--- src/components/home/About2SectionHome.tsx
+++ src/components/home/About2SectionHome.tsx
@@ -8,7 +8,7 @@ const ResortSectionFirst = () => {
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0)), url('./public/cta-bg.svg')`,
       }}
-      className="relative py-20 px-4 sm:px-6 bg-cover bg-center  bg-no-repeat"
+      className="relative py-20 px-4 sm:px-6 bg-cover bg-center bg-[url('./public/cta-bg.svg')]  bg-no-repeat"
     >
       {/* Dark overlay for better text contrast */}
       <div className="absolute inset-0 bg-black/40"></div>
